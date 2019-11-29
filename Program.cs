a0a92ae4e99ab9de90cc49ea6de14e9ec7c00b33
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-28 19:43:35 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace wizard_ninja_samurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Michelle = new Human("Michelle");
+            Wizard Bailey = new Wizard("Bailey");
+            Ninja Mark = new Ninja("Mark");
+            Samurai Kevin = new Samurai("Kevin");
+
+            while(Michelle.Health > 50)
+            {
+                Mark.Attack(Michelle);
+            }
+            Console.WriteLine($"Michelle's health is at {Michelle.Health}.");
+            Bailey.Heal(Michelle);
+        }
+    }
+}
